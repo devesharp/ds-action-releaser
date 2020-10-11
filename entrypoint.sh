bd5d7a57ebe48bded940bdbc5af12c25b4765c7e
Alberto ammar <albertoammarw@gmail.com>
2020-10-11 20:30:52 -0300

    feat: add token on password

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,9 +27,10 @@ fi
 
 cd ${INPUT_DIRECTORY}
 
-remote_repo="https://devesharp[bot]@users.noreply.github.com:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 
-git config --global user.name "devesharp"
-git config --global user.email "devesharp[bot]@users.noreply.github.com"
+git config user.name "devesharp"
+git config user.email "devesharp[bot]@users.noreply.github.com"
+git config user.password ${INPUT_GITHUB_TOKEN}
 
 git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
