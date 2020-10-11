35f307106450f2706524f8a847be63f094d7504a
Alberto ammar <albertoammarw@gmail.com>
2020-10-11 19:18:56 -0300

    chore: add ds-releaser to build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+npm install -g @devesharp/ds-releaser
+ds-releaser -t semantic --nonRoot
+
 INPUT_BRANCH=${INPUT_BRANCH:-master}
 INPUT_FORCE=${INPUT_FORCE:-false}
 INPUT_TAGS=${INPUT_TAGS:-false}
@@ -9,23 +12,21 @@ _FORCE_OPTION=''
 REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
 
 echo "Push to branch $INPUT_BRANCH";
-echo "Push to branch $INPUT_TAGS";
+[ -z "${INPUT_GITHUB_TOKEN}" ] && {
+    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+    exit 1;
+};
 
-# [ -z "${INPUT_GITHUB_TOKEN}" ] && {
-#     echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
-#     exit 1;
-# };
+if ${INPUT_FORCE}; then
+    _FORCE_OPTION='--force'
+fi
 
-# if ${INPUT_FORCE}; then
-#     _FORCE_OPTION='--force'
-# fi
+if ${INPUT_TAGS}; then
+    _TAGS='--tags'
+fi
 
-# if ${INPUT_TAGS}; then
-#     _TAGS='--tags'
-# fi
+cd ${INPUT_DIRECTORY}
 
-# cd ${INPUT_DIRECTORY}
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 
-# remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
-
-# git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
+git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
